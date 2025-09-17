75dd5d75f3792411fe69d49c5e760a762517e21b
aeo711 <adreaojukwu@gmail.com>
2025-09-17 17:32:05 -0500

    Added Ids to Divs and JS Moments

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
-alert("Hello World");
+function updateTime() {
+  // Los Angeles Content Block
+
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Paris Content Block
+
+  let parisTime = moment().tz("Europe/Paris");
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+setInterval(updateTime, 1000);
