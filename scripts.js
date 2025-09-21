4006c8de08ca38647c493ec096a439722b2b5ba7
aeo711 <adreaojukwu@gmail.com>
2025-09-21 15:51:10 -0500

    Added Option Value, to test JS Code Replace/Spit
    I added a City with words to test JS Code. The cityName variable replaces the underscore with a space in New York, and the .spilt removes the country. Country is {0} and {1} city in the JS Array.

--- scripts.js
+++ scripts.js
@@ -2,32 +2,38 @@ function updateTime() {
   // Los Angeles Code Block
 
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesElementDate = document.querySelector("#los-angeles .date");
-  let losAngelesElementTime = document.querySelector("#los-angeles .time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
-  losAngelesElementDate.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
-  losAngelesElementTime.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  if (losAngelesElement) {
+    let losAngelesElementDate = document.querySelector("#los-angeles .date");
+    let losAngelesElementTime = document.querySelector("#los-angeles .time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+    losAngelesElementDate.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
+    losAngelesElementTime.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   // Paris Code Block
-
   let parisElement = document.querySelector("#paris");
-  let parisElementDate = document.querySelector("#paris .date");
-  let parisElementTime = document.querySelector("#paris .time");
-  let parisTime = moment().tz("Europe/Paris");
-  parisElementDate.innerHTML = parisTime.format("MMMM Do, YYYY");
-  parisElementTime.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+  if (parisElement) {
+    let parisElementDate = document.querySelector("#paris .date");
+    let parisElementTime = document.querySelector("#paris .time");
+    let parisTime = moment().tz("Europe/Paris");
+    parisElementDate.innerHTML = parisTime.format("MMMM Do, YYYY");
+    parisElementTime.innerHTML = parisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
     <div class="city">
     <div>
-      <h2>${cityTimeZone}</h2>
+      <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
     </div>
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
