f56d0de8514b4029dcaf59c5b9baaf6e8b464705
aeo711 <adreaojukwu@gmail.com>
2025-09-21 14:47:39 -0500

    JS inner.HTML Debugged.
    Select Element is now working properly.

--- scripts.js
+++ scripts.js
@@ -24,15 +24,15 @@ function updateCity(event) {
   let cityTimeZone = event.target.value;
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = cityTimeZone;
-  citiesElement = `<div class="city">
+  citiesElement.innerHTML = `
+    <div class="city">
     <div>
       <h2>${cityTimeZone}</h2>
       <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
     </div>
-    <div class="time">${cityTime.format("h:mm:ss")} [<small]>${cityTime.format(
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
     "A"
-  )}[</small>])}</div>
+  )}</small></div>
   </div>`;
 }
 
