829fab0ba46681b0041d9e7d8a9e606329dbe6a3
aeo711 <adreaojukwu@gmail.com>
2025-09-21 11:12:56 -0500

    JS File Recoded and Debugged
    function updateTime added back to file.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  // Los Angeles Code Block
 
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesElementDate = document.querySelector("#los-angeles .date");
+  let losAngelesElementTime = document.querySelector("#los-angeles .time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesElementDate.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
+  losAngelesElementTime.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Paris Code Block
+
+  let parisElement = document.querySelector("#paris");
+  let parisElementDate = document.querySelector("#paris .date");
+  let parisElementTime = document.querySelector("#paris .time");
+  let parisTime = moment().tz("Europe/Paris");
+  parisElementDate.innerHTML = parisTime.format("MMMM Do, YYYY");
+  parisElementTime.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
