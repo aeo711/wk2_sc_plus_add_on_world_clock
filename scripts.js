766c5b8a51c6e7e919d7c0eed3a60e6a51d76bc9
aeo711 <adreaojukwu@gmail.com>
2025-09-21 14:18:10 -0500

    JS Select Element Coded. Debbing inner.HTML

--- scripts.js
+++ scripts.js
@@ -19,5 +19,25 @@ function updateTime() {
   parisElementDate.innerHTML = parisTime.format("MMMM Do, YYYY");
   parisElementTime.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = cityTimeZone;
+  citiesElement = `<div class="city">
+    <div>
+      <h2>${cityTimeZone}</h2>
+      <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} [<small]>${cityTime.format(
+    "A"
+  )}[</small>])}</div>
+  </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
